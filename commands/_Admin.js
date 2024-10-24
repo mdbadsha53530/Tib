13ca82244b8ca2738f06944b5c0028d957dfccee
Bots.Business <git.sync@bots.business>
2024-10-24 16:26:41 +0000

    Bots.Business export for bot id: 1722570

---
+++ commands/_Admin.js
@@ -0,0 +1,12 @@
+/*CMD
+  command: /Admin
+  help: 
+  need_reply: true
+  auto_retry_time: 
+  folder: 
+  answer: https://t.me/LegitBotsJR
+  keyboard: 
+  aliases: 🔰admin official channel
+  group: 
+CMD*/
+
